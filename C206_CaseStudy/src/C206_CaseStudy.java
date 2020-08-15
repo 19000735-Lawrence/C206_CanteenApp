8784c09f7e0bafa83c3ae39bf7373ac750a26a30
19000735 <19000735@PFT-19000735.rp.edu.sg>
2020-08-15 15:03:07 +0800

    Created Main Class Skeleton Structure

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,94 @@
 
 public class C206_CaseStudy {
+	
+	private static final int CUSTOMER = 1;
+	private static final int STALL_STAFF = 2;
+	private static final int CANTEEN_ADMIN = 3;
+	
+	private static final int OPTION_MENUITEM = 1;
+	private static final int OPTION_ACCOUNT = 2;
+	private static final int OPTION_ORDER = 3;
+	private static final int OPTION_PURCHASEORDER = 4;
+	private static final int OPTION_PROMOTION = 5;
+	private static final int OPTION_USERTYPE = 6;
+	private static final int OPTION_EXIT = 7;
+	
+	public static int userOption = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int option = 0;
+		
+		while(userOption != CUSTOMER && userOption != STALL_STAFF && userOption != CANTEEN_ADMIN) {
+			userTypeMenu();
+			userOption = Helper.readInt("Enter User type > ");
+			if(userOption != CUSTOMER && userOption != STALL_STAFF && userOption != CANTEEN_ADMIN) {
+				System.out.println("Please enter valid user type!");
+			}
+		}
+		
+		while (option != 6) {
+			
+			optionMenu();
+			option = Helper.readInt("Enter an option > ");
+			
+			if(option == OPTION_MENUITEM) {
+				//Do code for Menu Item here
+				
+			} else if(option == OPTION_ACCOUNT) {
+				//Do code for Account here
+				
+			} else if(option == OPTION_ORDER) {
+				//Do code for Order here
+				
+			} else if(option == OPTION_PURCHASEORDER) {
+				//Do code for Purchase Order here
+				
+			} else if(option == OPTION_PROMOTION) {
+				//Do code for Promotion here
+				
+			} else if(option == OPTION_USERTYPE) {
+				while(userOption != CUSTOMER && userOption != STALL_STAFF && userOption != CANTEEN_ADMIN) {
+					userTypeMenu();
+					userOption = Helper.readInt("Enter User type > ");
+					if(userOption != CUSTOMER && userOption != STALL_STAFF && userOption != CANTEEN_ADMIN) {
+						System.out.println("Please enter valid user type!");
+					}
+				}
+			} else if(option == OPTION_EXIT) {
+				System.out.println("Goodbye!");
+			} else {
+				System.out.println("Invalid choice!");
+			}
+				
+		}
 	}
-
+	
+	public static void userTypeMenu() {
+		String output = String.format("%-5s%-20s", "No.", "User Type");
+		output += String.format("\n%-5s%-20s", "===", "=========");
+		output += String.format("\n%-5d%-20s", CUSTOMER, "Customer");
+		output += String.format("\n%-5d%-20s", STALL_STAFF, "Stall staff");
+		output += String.format("\n%-5d%-20s", CANTEEN_ADMIN, "Canteen Admin");
+		System.out.println(output);
+	}
+	
+	public static void optionMenu() {
+		String output = String.format("%-5s%-20s", "No.", "Menu Choice");
+		output += String.format("\n%-5s%-20s", "===", "=========");
+		output += String.format("\n%-5d%-20s", 1, "Menu Item Options");
+		output += String.format("\n%-5d%-20s", 2, "Account Options");
+		output += String.format("\n%-5d%-20s", 3, "Order Options");
+		output += String.format("\n%-5d%-20s", 4, "Purchase Order Options");
+		output += String.format("\n%-5d%-20s", 5, "Promotion Options");
+		output += String.format("\n%-5d%-20s", 6, "Change User Type");
+		output += String.format("\n%-5d%-20s", 7, "Quit");
+		System.out.println(output);
+	}
+	
+	//Create all the methods here \/ \/ \/
+	
+	
+	
 }
