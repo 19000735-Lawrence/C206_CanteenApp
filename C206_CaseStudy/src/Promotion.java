9b7f15a2e76ac9992a92461fc9de7c3bc1932f91
ys2409 <19022397@myrp.edu.sg>
2020-08-21 11:43:50 +0800

    Promotion.java done

--- C206_CaseStudy/src/Promotion.java
+++ C206_CaseStudy/src/Promotion.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
 public class Promotion {
 
+	private String status;
+	private ArrayList<PromotionItem> items;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public ArrayList<PromotionItem> getItems() {
+		return items;
+	}
+	public void setItems(ArrayList<PromotionItem> items) {
+		this.items = items;
+	}
+	@Override
+	public String toString() {
+		return "Promotion [status=" + status + "]";
+	}
+	
+	
+	
 }
