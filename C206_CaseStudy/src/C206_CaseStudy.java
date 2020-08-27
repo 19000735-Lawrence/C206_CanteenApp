4c2d45c7dabbdff537a8655da1e6a8a642785ed5
19013886 <19013886@DESKTOP-T6LSKLQ>
2020-08-27 22:44:02 +0800

    Update codes for account

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -553,7 +553,7 @@ public class C206_CaseStudy {
 		Helper.line(111, "-");
 		System.out.println(output);
 		for (int i = 0; i < account.size(); i++) {
-			output += String.format("%-15s %-15d %-15d %-15s\n", account.get(i).getuserRole(),
+			output += String.format("%-15s %-15s %-15s %-15s\n", account.get(i).getuserRole(),
 					account.get(i).getcontactNumber(), account.get(i).getstudentID(), account.get(i).getusername());
 			output += String.format("%" + 111 + "s", " ").replaceAll(" ", "-");
 		}
